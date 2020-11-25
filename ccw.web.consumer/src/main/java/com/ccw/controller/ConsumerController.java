6c2bfa2663b62304c1e341cd25e7ead093c92d30
ccw924263 <ccw924263059>
2020-11-26 01:03:57 +0800

    nacos

--- ccw.web.consumer/src/main/java/com/ccw/controller/ConsumerController.java
+++ ccw.web.consumer/src/main/java/com/ccw/controller/ConsumerController.java
@@ -1,5 +1,13 @@
 package com.ccw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 @RestController
 public class ConsumerController {
 
