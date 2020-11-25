4658a7480585fb8d24c2cd81e896c2527a7f4259
ccw924263 <ccw924263059>
2020-11-26 00:53:33 +0800

    nacos

--- ccw.web.consumer/src/main/java/com/ccw/ConsumerApplication.java
+++ ccw.web.consumer/src/main/java/com/ccw/ConsumerApplication.java
@@ -4,6 +4,7 @@ import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
